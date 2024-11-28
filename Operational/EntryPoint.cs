a3d28b5f5b264fd430579d6d92161b2fdd290f52
Cyrus <88629892+RefGnom@users.noreply.github.com>
2024-11-28 14:57:01 +0500

    entry point wip

---
+++ Operational/EntryPoint.cs
@@ -0,0 +1,20 @@
+﻿using Core.IO;
+using Core.Logging;
+using Core.Providers;
+using Operational.RegularProcesses;
+using Operational.RegularProcesses.ProcessCore;
+
+namespace Operational;
+
+public static class EntryPoint
+{
+    public static async Task Main()
+    {
+        var processRunner = new ProcessRunner();
+        var logFactory = new LogFactory(new DateTimeProvider(), new Writer());
+        var process = new ExpiredEventsDetectProcess(logFactory);
+        processRunner.RegisterProcess(process);
+        await processRunner.RunAsync();
+        Console.ReadLine();
+    }
+}
