b9df2e817f4093b9c262b273dbe06cd02b749d9a
Cyrus <88629892+RefGnom@users.noreply.github.com>
2024-11-28 14:55:05 +0500

    regular processes core

---
+++ Operational/RegularProcesses/ProcessCore/IProcessRunner.cs
@@ -0,0 +1,9 @@
+﻿namespace Operational.RegularProcesses.ProcessCore;
+
+public interface IProcessRunner
+{
+    void RegisterProcess(IRegularProcess regularProcess);
+    Task RunAsync();
+    Task StartProcessAsync(string processName, bool isRepeat = false);
+    Task StopProcessAsync(string processName);
+}
---
+++ Operational/RegularProcesses/ProcessCore/IRegularProcess.cs
@@ -0,0 +1,9 @@
+﻿namespace Operational.RegularProcesses.ProcessCore;
+
+public interface IRegularProcess
+{
+    string ProcessName { get; }
+    bool IsActiveByDefault { get; }
+    Task StartAsync(bool isRepeat = true);
+    Task StopAsync();
+}
