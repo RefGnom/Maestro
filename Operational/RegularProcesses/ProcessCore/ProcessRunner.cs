fc5ba4f2c3ac2c762bdcfc57fc72b3a1b90f4753
Cyrus <88629892+RefGnom@users.noreply.github.com>
2024-11-28 14:55:05 +0500

    regular processes core

---
+++ Operational/RegularProcesses/ProcessCore/ProcessRunner.cs
@@ -0,0 +1,32 @@
+﻿namespace Operational.RegularProcesses.ProcessCore;
+
+public class ProcessRunner : IProcessRunner
+{
+    private readonly List<IRegularProcess> _regularProcesses = [];
+
+    public void RegisterProcess(IRegularProcess regularProcess)
+    {
+        _regularProcesses.Add(regularProcess);
+    }
+
+    public async Task RunAsync()
+    {
+        var activeByDefaultRegularProcess = _regularProcesses.Where(regularProcess => regularProcess.IsActiveByDefault);
+        foreach (var regularProcess in activeByDefaultRegularProcess)
+        {
+            await regularProcess.StartAsync();
+        }
+    }
+
+    public async Task StartProcessAsync(string processName, bool isRepeat = false)
+    {
+        var regularProcess = _regularProcesses.Single(x => x.ProcessName == processName);
+        await regularProcess.StartAsync(isRepeat);
+    }
+
+    public async Task StopProcessAsync(string processName)
+    {
+        var regularProcess = _regularProcesses.Single(x => x.ProcessName == processName);
+        await regularProcess.StopAsync();
+    }
+}
---
+++ Operational/RegularProcesses/ProcessCore/RegularProcessBase.cs
@@ -0,0 +1,49 @@
+﻿using System.Timers;
+using Core.Logging;
+using Timer = System.Timers.Timer;
+
+namespace Operational.RegularProcesses.ProcessCore;
+
+public abstract class RegularProcessBase : IRegularProcess
+{
+    private readonly Timer _timer;
+    private readonly ILog _log;
+
+    protected RegularProcessBase(ILog log)
+    {
+        _log = log;
+        _timer = new Timer();
+        _timer.Elapsed += TimerOnElapsed;
+    }
+
+
+    public abstract string ProcessName { get; }
+    public abstract bool IsActiveByDefault { get; }
+    protected abstract TimeSpan Timeout { get; }
+
+    public abstract Task RunAsync();
+
+    public Task StartAsync(bool isRepeat = true)
+    {
+        _log.Info($"Starting regular process, IsRepeat: {isRepeat}");
+        _timer.Interval = Timeout.TotalMilliseconds;
+        _timer.AutoReset = isRepeat;
+        _timer.Start();
+
+        return Task.CompletedTask;
+    }
+
+    private void TimerOnElapsed(object? sender, ElapsedEventArgs e)
+    {
+        _log.Info("Running regular process");
+        RunAsync().GetAwaiter().GetResult();
+    }
+
+    public Task StopAsync()
+    {
+        _log.Info("Stoping regular process");
+        _timer.Stop();
+
+        return Task.CompletedTask;
+    }
+}
