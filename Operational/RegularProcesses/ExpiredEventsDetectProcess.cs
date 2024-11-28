bbd7ac2bd06719f22a3a77cc0cece1aac1f5979c
Cyrus <88629892+RefGnom@users.noreply.github.com>
2024-11-28 14:55:24 +0500

    ExpiredEventsDetectProcess Stub

---
+++ Operational/RegularProcesses/ExpiredEventsDetectProcess.cs
@@ -0,0 +1,17 @@
+﻿using Core.Logging;
+using Operational.RegularProcesses.ProcessCore;
+
+namespace Operational.RegularProcesses;
+
+public class ExpiredEventsDetectProcess(ILogFactory logFactory) : RegularProcessBase(logFactory.ForContext<ExpiredEventsDetectProcess>())
+{
+    public override string ProcessName => "Процесс по очищению истёкших эвентов";
+    public override bool IsActiveByDefault => true;
+    protected override TimeSpan Timeout => TimeSpan.FromSeconds(3);
+
+    public override Task RunAsync()
+    {
+        Console.WriteLine("Эвенты очищенны");
+        return Task.CompletedTask;
+    }
+}
