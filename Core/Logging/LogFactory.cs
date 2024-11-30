d0b388f991fb3ff9295b1b0a5612c6e649b8816f
Cyrus <88629892+RefGnom@users.noreply.github.com>
2024-11-30 16:29:04 +0500

    Исправил, чтобы не обязательно было добавлять файл с настройками

--- Core/Logging/LogFactory.cs
+++ Core/Logging/LogFactory.cs
@@ -5,6 +5,9 @@ namespace Maestro.Core.Logging;
 
 public class LogFactory(IDateTimeProvider dateTimeProvider, IWriter writer) : ILogFactory
 {
+    [Obsolete("Не использовать. Нужен для логов на этапе конфигурации")]
+    public static LogFactory ClosedFactory => new(new DateTimeProvider(), new Writer());
+
     public ILog<T> ForContext<T>()
     {
         return new Log<T>(dateTimeProvider, writer);
