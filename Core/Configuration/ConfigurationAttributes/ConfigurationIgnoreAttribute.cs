d8dc32d55d6b8c9b69b2fe44730964fdba5c0e37
Cyrus <88629892+RefGnom@users.noreply.github.com>
2024-11-30 14:53:49 +0500

    configuration attributes

---
+++ Core/Configuration/ConfigurationAttributes/ConfigurationIgnoreAttribute.cs
@@ -0,0 +1,6 @@
+﻿namespace Maestro.Core.Configuration.ConfigurationAttributes;
+
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Interface)]
+public class ConfigurationIgnoreAttribute : Attribute
+{
+}
---
+++ Core/Configuration/ConfigurationAttributes/ConfigurationScopeAttribute.cs
@@ -0,0 +1,9 @@
+﻿using SimpleInjector;
+
+namespace Maestro.Core.Configuration.ConfigurationAttributes;
+
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Interface)]
+public class ConfigurationScopeAttribute(Lifestyle lifestyle) : Attribute
+{
+    public Lifestyle Lifestyle { get; } = lifestyle;
+}
