31a6803e6b2984598071ca29f7aa70b899b2e35f
Cyrus <88629892+RefGnom@users.noreply.github.com>
2024-11-30 14:54:37 +0500

    auto registration

---
+++ Core/Configuration/DependencyInjectionConfiguration/DependencyInjectionConfigurator.cs
@@ -0,0 +1,51 @@
+﻿using Maestro.Core.Providers;
+using Microsoft.Extensions.Configuration;
+using SimpleInjector;
+
+namespace Maestro.Core.Configuration.DependencyInjectionConfiguration;
+
+public static class DependencyInjectionConfigurator
+{
+    private const string settingsName = "appsettings.json";
+    private static readonly Lifestyle LifestyleByDefault = Lifestyle.Singleton;
+
+    public static Container Configure(this Container container)
+    {
+        var assemblies = AssemblyHelper.GetServiceAssemblies();
+
+        var registrations = assemblies
+            .SelectMany(x => x.GetExportedTypes())
+            .Where(x => !x.IsInterface)
+            .Where(x => !x.IsAbstract)
+            .SelectMany(
+                x => x.GetInterfaces()
+                    .Where(i => assemblies.Contains(i.Assembly))
+                    .Select(i => new Registration(i, x, LifestyleByDefault))
+            )
+            .Where(x => x.Resolve())
+            .Select(x => x.GetGenericDefinitionTypeIfNeed())
+            .ToArray();
+
+        foreach (var registration in registrations)
+        {
+            var scopeAttribute = registration.GetScopeAttribute();
+            container.Register(
+                registration.InterfaceType,
+                registration.ImplementationType,
+                scopeAttribute?.Lifestyle ?? registration.Lifestyle
+            );
+        }
+
+        return container;
+    }
+
+    public static Container ConfigureSettings(this Container container)
+    {
+        var configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile(settingsName, optional: false, reloadOnChange: true)
+            .Build();
+        container.RegisterInstance<ISettingsProvider>(new SettingsProvider(configuration));
+        return container;
+    }
+}
---
+++ Core/Configuration/DependencyInjectionConfiguration/RegistrationExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System.Reflection;
+using Maestro.Core.Configuration.ConfigurationAttributes;
+
+namespace Maestro.Core.Configuration.DependencyInjectionConfiguration;
+
+public static class RegistrationExtensions
+{
+    public static Registration GetGenericDefinitionTypeIfNeed(this Registration registration)
+    {
+        if (registration.InterfaceType.IsGenericType && registration.ImplementationType.IsGenericType)
+        {
+            return new Registration(
+                registration.InterfaceType.GetGenericTypeDefinition(),
+                registration.ImplementationType.GetGenericTypeDefinition(),
+                registration.Lifestyle
+            );
+        }
+
+        return registration;
+    }
+
+    public static ConfigurationScopeAttribute? GetScopeAttribute(this Registration registration)
+    {
+        return registration.ImplementationType.GetCustomAttribute<ConfigurationScopeAttribute>()
+               ?? registration.InterfaceType.GetCustomAttribute<ConfigurationScopeAttribute>();
+    }
+}
