37db9f5d3379a1d2b9118ccd22b67e30247be581
Cyrus <88629892+RefGnom@users.noreply.github.com>
2024-12-02 21:34:03 +0500

    И снова логер

--- Core/Logging/ILog.cs
+++ Core/Logging/ILog.cs
@@ -6,3 +6,10 @@ public interface ILog<TContext>
     void Warn(string message);
     void Error(string message);
 }
+
+public interface ILog
+{
+    void Info(string message);
+    void Warn(string message);
+    void Error(string message);
+}
