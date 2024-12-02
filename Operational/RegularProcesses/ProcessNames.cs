271ba8ca3a508676de722a56e1faf22a0fbea59b
Cyrus <88629892+RefGnom@users.noreply.github.com>
2024-12-02 22:00:27 +0500

    cleanup

---
+++ Operational/RegularProcesses/ProcessNames.cs
@@ -0,0 +1,6 @@
+﻿namespace Maestro.Operational.RegularProcesses;
+
+public static class ProcessNames
+{
+    public const string DetectExpiredEventsProcessName = "Процесс по очищению истёкших эвентов";
+}
