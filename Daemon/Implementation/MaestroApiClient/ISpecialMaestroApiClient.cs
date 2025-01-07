beaa591a8631fd33c6471a170dd37c69605dcdc3
Cyrus <88629892+RefGnom@users.noreply.github.com>
2025-01-07 18:57:53 +0500

    add api client for daemon

---
+++ Daemon/Implementation/MaestroApiClient/ISpecialMaestroApiClient.cs
@@ -0,0 +1,10 @@
+﻿using Maestro.Server.Core.Models;
+
+namespace Daemon.Implementation.MaestroApiClient;
+
+public interface ISpecialMaestroApiClient
+{
+    public IAsyncEnumerable<ReminderWithIdDto> GetCompletedRemindersAsync();
+    public IAsyncEnumerable<ReminderWithIdDto> GetRemindersAsync(DateTime exclusiveStartDateTime, DateTime inclusiveEndDateTime);
+    public Task DeleteReminder(long reminderId);
+}
