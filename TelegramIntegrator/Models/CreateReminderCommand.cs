d1f4c1b1e63463707026ccc6446da85616dea7df
Cyrus <88629892+RefGnom@users.noreply.github.com>
2025-01-08 21:38:31 +0500

    clean up

---
+++ TelegramIntegrator/Models/CreateReminderCommand.cs
@@ -0,0 +1,8 @@
+﻿namespace Maestro.TelegramIntegrator.Models;
+
+public class CreateReminderCommand(string command, string description, DateTime reminderTime) : ICommand
+{
+    public string Command { get; } = command;
+    public string Description { get; } = description;
+    public DateTime ReminderTime { get; } = reminderTime;
+}
---
+++ TelegramIntegrator/Models/ParseResult.cs
@@ -0,0 +1,11 @@
+﻿namespace Maestro.TelegramIntegrator.Models;
+
+public class ParseResult(bool isSuccessful, string parseFailureMessage, ICommand command)
+{
+    public bool IsSuccessful { get; } = isSuccessful;
+    public string ParseFailureMessage { get; } = parseFailureMessage;
+    public ICommand Command { get; } = command;
+
+    public static ParseResult CreateSuccess(ICommand command) => new(true, null!, command);
+    public static ParseResult CreateFailure(string parseFailureMessage) => new(false, parseFailureMessage, null!);
+}
