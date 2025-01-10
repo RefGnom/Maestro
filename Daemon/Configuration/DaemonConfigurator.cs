bfb5b975077390ec75a8c35f106dea47d55c750e
Cyrus <88629892+RefGnom@users.noreply.github.com>
2025-01-11 01:19:24 +0500

    Регистрация апи клиента

--- Daemon/Configuration/DaemonConfigurator.cs
+++ Daemon/Configuration/DaemonConfigurator.cs
@@ -1,7 +1,23 @@
-﻿using Maestro.Core.Configuration;
+﻿using Maestro.Client.Daemon;
+using Maestro.Core.Configuration;
+using Maestro.Core.Providers;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Maestro.Daemon.Configuration;
 
 public class DaemonConfigurator : ConfiguratorBase
 {
+    protected override void Customize(IServiceCollection container)
+    {
+        container.AddSingleton<IDaemonMaestroApiClient>(
+            serviceProvider =>
+            {
+                var maestroClientFactory = serviceProvider.GetRequiredService<IDaemonMaestroApiClientFactory>();
+                var settingsProvider = serviceProvider.GetRequiredService<ISettingsProvider>();
+                var uri = settingsProvider.Get("MaestroDaemonUri");
+                var apiKey = settingsProvider.Get("MaestroDaemonApiKey");
+                return maestroClientFactory.Create(uri, apiKey);
+            }
+        );
+    }
 }
