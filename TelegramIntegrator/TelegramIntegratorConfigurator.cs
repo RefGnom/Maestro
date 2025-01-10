354e265d2d82468cf3c7eb731d27e3c640e079f3
Cyrus <88629892+RefGnom@users.noreply.github.com>
2025-01-11 00:07:34 +0500

    комменты)

--- TelegramIntegrator/TelegramIntegratorConfigurator.cs
+++ TelegramIntegrator/TelegramIntegratorConfigurator.cs
@@ -1,4 +1,5 @@
-﻿using Maestro.Core.Configuration;
+﻿using Maestro.Client.Integrator;
+using Maestro.Core.Configuration;
 using Maestro.Core.Providers;
 using Microsoft.Extensions.DependencyInjection;
 using Telegram.Bot;
@@ -12,15 +13,15 @@ public class TelegramIntegratorConfigurator : ConfiguratorBase
         container.AddSingleton<ITelegramBotClient>(
             s => new TelegramBotClient(s.GetRequiredService<ISettingsProvider>().Get("TelegramBotToken"))
         );
-        //container.AddSingleton<IMaestroApiClient>(
-        //    serviceProvider =>
-        //    {
-        //        var maestroClientFactory = serviceProvider.GetRequiredService<IMaestroApiClientFactory>();
-        //        var settingsProvider = serviceProvider.GetRequiredService<ISettingsProvider>();
-        //        var uri = settingsProvider.Get("MaestroUri");
-        //        var apiKey = settingsProvider.Get("MaestroApiKey");
-        //        return maestroClientFactory.Create(uri, apiKey);
-        //    }
-        //);
+        container.AddSingleton<IMaestroApiClient>(
+            serviceProvider =>
+            {
+                var maestroClientFactory = serviceProvider.GetRequiredService<IMaestroApiClientFactory>();
+                var settingsProvider = serviceProvider.GetRequiredService<ISettingsProvider>();
+                var uri = settingsProvider.Get("MaestroUri");
+                var apiKey = settingsProvider.Get("MaestroApiKey");
+                return maestroClientFactory.Create(uri, apiKey);
+            }
+        );
     }
 }
