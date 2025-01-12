ea2e527ac098ea7a3fe6ad5d1f75843e0f0e2011
nastyuxxxa <nastya-kubar@ya.ru>
2025-01-12 20:29:02 +0600

    тесты

--- TelegramIntegrator/Implementation/View/TelegramMessageBuilder.cs
+++ TelegramIntegrator/Implementation/View/TelegramMessageBuilder.cs
@@ -6,6 +6,8 @@ public static class TelegramMessageBuilder
 {
     public static string BuildByCommandPattern(string telegramCommandPattern) => $"Пожалуйста, введите сообщение по шаблону:\n\n\"{telegramCommandPattern}\"";
 
+    public static string BuildParseFailureMessage(string parseFailureMessage) => $"Не удалось распарсить: {parseFailureMessage}";
+
     public static string BuildUnknownCommand()
     {
         return $"Не смогли распознать введёную вами команду. Список возможных команд:" +
