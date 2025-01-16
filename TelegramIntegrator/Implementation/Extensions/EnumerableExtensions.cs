2c845ab3421c780213ef7a374d47288d26dd21ab
Cyrus <88629892+RefGnom@users.noreply.github.com>
2025-01-17 00:32:30 +0500

    extensions

---
+++ TelegramIntegrator/Implementation/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,9 @@
+﻿namespace Maestro.TelegramIntegrator.Implementation.Extensions;
+
+public static class EnumerableExtensions
+{
+    public static string JoinToString<T>(this IEnumerable<T> source, string separator)
+    {
+        return string.Join(separator, source);
+    }
+}
---
+++ TelegramIntegrator/Implementation/Extensions/LogExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System.Diagnostics;
+using Maestro.Core.Logging;
+
+namespace Maestro.TelegramIntegrator.Implementation.Extensions;
+
+public static class LogExtensions
+{
+    public static void WarnWithStackTrace<T>(this ILog<T> log, string message, int traceCount)
+    {
+        const string separator = "\n";
+        var trace = new StackTrace().ToString().Split(separator).Take(traceCount).JoinToString(separator);
+        log.Warn($"{message}{separator}{trace}");
+    }
+}
