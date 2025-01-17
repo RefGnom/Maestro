d316ad1570e36aaf6690706e5c2d440261735ddf
Cyrus <88629892+RefGnom@users.noreply.github.com>
2025-01-17 09:21:17 +0500

    UserDateTimeService

---
+++ TelegramIntegrator/Implementation/IUserDateTimeService.cs
@@ -0,0 +1,9 @@
+﻿using Telegram.Bot.Types;
+
+namespace Maestro.TelegramIntegrator.Implementation;
+
+public interface IUserDateTimeService
+{
+    bool TryGetUserDateTime(long userId, out DateTime? userDateTime);
+    void SetUserLocation(long userId, Location location);
+}
---
+++ TelegramIntegrator/Implementation/UserDateTimeService.cs
@@ -0,0 +1,29 @@
+﻿using GeoTimeZone;
+using Telegram.Bot.Types;
+using TimeZoneConverter;
+
+namespace Maestro.TelegramIntegrator.Implementation;
+
+public class UserDateTimeService : IUserDateTimeService
+{
+    private readonly Dictionary<long, TimeZoneInfo> _timeZones = new();
+
+    public bool TryGetUserDateTime(long userId, out DateTime? userDateTime)
+    {
+        if (_timeZones.TryGetValue(userId, out var timeZoneInfo))
+        {
+            userDateTime = TimeZoneInfo.ConvertTime(DateTime.UtcNow, timeZoneInfo);
+            return true;
+        }
+
+        userDateTime = null;
+        return false;
+    }
+
+    public void SetUserLocation(long userId, Location location)
+    {
+        var timeZone = TimeZoneLookup.GetTimeZone(location.Latitude, location.Longitude).Result;
+        var timeZoneInfo = TZConvert.GetTimeZoneInfo(timeZone);
+        _timeZones[userId] = timeZoneInfo;
+    }
+}
