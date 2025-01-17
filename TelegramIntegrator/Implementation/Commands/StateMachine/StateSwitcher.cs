506697573b9225dd6794a0859a45f37f279773ac
Cyrus <88629892+RefGnom@users.noreply.github.com>
2025-01-17 18:38:46 +0500

    Накинул лог

--- TelegramIntegrator/Implementation/Commands/StateMachine/StateSwitcher.cs
+++ TelegramIntegrator/Implementation/Commands/StateMachine/StateSwitcher.cs
@@ -1,19 +1,22 @@
-﻿using Microsoft.Extensions.Caching.Memory;
+﻿using Maestro.Core.Logging;
+using Microsoft.Extensions.Caching.Memory;
 
 namespace Maestro.TelegramIntegrator.Implementation.Commands.StateMachine;
 
-public class StateSwitcher(Lazy<IStatesProvider> statesProvider) : IStateSwitcher
+public class StateSwitcher(Lazy<IStatesProvider> statesProvider, ILog<StateSwitcher> log) : IStateSwitcher
 {
     private static readonly TimeSpan CacheExpirationTimeout = TimeSpan.FromMinutes(15);
 
     private readonly MemoryCache _memoryCache = new(new MemoryCacheOptions());
     private readonly Lazy<IStatesProvider> _statesProvider = statesProvider;
+    private readonly ILog<StateSwitcher> _log = log;
 
     public async Task SetStateAsync<TState>(long userId) where TState : IState
     {
         var state = _statesProvider.Value.GetState<TState>();
         _memoryCache.Set(userId, state, CacheExpirationTimeout);
         await state.Initialize(userId);
+        _log.Info($"Для юзера {userId} установлено состояние {typeof(TState).Name}");
     }
 
     public IState GetState(long userId)
